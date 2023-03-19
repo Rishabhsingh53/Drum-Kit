954e7e7b8b034cbff310bdfb846e5aa42896c12c
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-19 21:05:08 +0530

    Using switch just on click event

--- index.js
+++ index.js
@@ -10,35 +10,37 @@ function () {
 
 for (let i = 0 ; i < document.querySelectorAll("button.drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click",function () {
-        if (i == 0) { 
-            var audio = new Audio("sounds/w.mp3")
-            audio.play();
-        }
-        else if (i == 1) {
-            var audio = new Audio("sounds/a.mp3")
-            audio.play();
-        }
-        else if (i == 2) {
-            var audio = new Audio("sounds/s.mp3")
-            audio.play();
-        }
-        else if (i == 3) {
-            var audio = new Audio("sounds/d.mp3")
-            audio.play();
-        }
-        else if (i == 4 ) {
-            var audio = new Audio("sounds/j.mp3")
-            audio.play();
-        }
-        else if (i == 5 ) {
-            var audio = new Audio("sounds/k.mp3")
-            audio.play();
-        }
-        else if (i == 6) {
-            var audio = new Audio("sounds/l.mp3")
-            audio.play();
+        var whichDrumToPlay = this.textContent;
+
+        switch (whichDrumToPlay) {
+            case "w": 
+                var audio = new Audio("sounds/w.mp3")
+                audio.play()
+                break
+            case "s": 
+                var audio = new Audio("sounds/s.mp3")
+                audio.play()
+                break
+            case "a": 
+                var audio = new Audio("sounds/a.mp3")
+                audio.play()
+                break
+            case "d": 
+                var audio = new Audio("sounds/d.mp3")
+                audio.play()
+                break
+            case "j": 
+                var audio = new Audio("sounds/j.mp3")
+                audio.play()
+                break
+            case "k": 
+                var audio = new Audio("sounds/k.mp3")
+                audio.play()
+                break
+            case "l": 
+                var audio = new Audio("sounds/l.mp3")
+                audio.play()
+                break
         }
     })
-    // for playing sound 
-    var audio = new Audio("sounds/w.mp3");
 }
