57458ff090bee2c9defb88a0b5f0b86201a98906
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-19 21:56:37 +0530

    Done using keyboard as well as button click

--- index.js
+++ index.js
@@ -11,36 +11,46 @@ function () {
 for (let i = 0 ; i < document.querySelectorAll("button.drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click",function () {
         var whichDrumToPlay = this.textContent;
-
-        switch (whichDrumToPlay) {
-            case "w": 
-                var audio = new Audio("sounds/w.mp3")
-                audio.play()
-                break
-            case "s": 
-                var audio = new Audio("sounds/s.mp3")
-                audio.play()
-                break
-            case "a": 
-                var audio = new Audio("sounds/a.mp3")
-                audio.play()
-                break
-            case "d": 
-                var audio = new Audio("sounds/d.mp3")
-                audio.play()
-                break
-            case "j": 
-                var audio = new Audio("sounds/j.mp3")
-                audio.play()
-                break
-            case "k": 
-                var audio = new Audio("sounds/k.mp3")
-                audio.play()
-                break
-            case "l": 
-                var audio = new Audio("sounds/l.mp3")
-                audio.play()
-                break
-        }
+        playSound(whichDrumToPlay)
     })
+}
+
+document.addEventListener("keypress",function (event) {
+    playSound(event.key)
+})
+
+function playSound(whichDrumToPlay) {
+    
+    switch (whichDrumToPlay) {
+        case "w": 
+            var audio = new Audio("sounds/w.mp3")
+            audio.play()
+            break
+        case "s": 
+            var audio = new Audio("sounds/s.mp3")
+            audio.play()
+            break
+        case "a": 
+            var audio = new Audio("sounds/a.mp3")
+            audio.play()
+            break
+        case "d": 
+            var audio = new Audio("sounds/d.mp3")
+            audio.play()
+            break
+        case "j": 
+            var audio = new Audio("sounds/j.mp3")
+            audio.play()
+            break
+        case "k": 
+            var audio = new Audio("sounds/k.mp3")
+            audio.play()
+            break
+        case "l": 
+            var audio = new Audio("sounds/l.mp3")
+            audio.play()
+            break
+        default :
+            console.log(whichDrumToPlay)
+    }
 }
