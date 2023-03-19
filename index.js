570986d8594c4dbb5a202d28d74a76d511c422af
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-19 20:52:51 +0530

    Implementation using clicks

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ function () {
 
 for (let i = 0 ; i < document.querySelectorAll("button.drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click",function () {
-        this.style.color = "white";
+        if (i == 0) { 
+            var audio = new Audio("sounds/w.mp3")
+            audio.play();
+        }
+        else if (i == 1) {
+            var audio = new Audio("sounds/a.mp3")
+            audio.play();
+        }
+        else if (i == 2) {
+            var audio = new Audio("sounds/s.mp3")
+            audio.play();
+        }
+        else if (i == 3) {
+            var audio = new Audio("sounds/d.mp3")
+            audio.play();
+        }
+        else if (i == 4 ) {
+            var audio = new Audio("sounds/j.mp3")
+            audio.play();
+        }
+        else if (i == 5 ) {
+            var audio = new Audio("sounds/k.mp3")
+            audio.play();
+        }
+        else if (i == 6) {
+            var audio = new Audio("sounds/l.mp3")
+            audio.play();
+        }
     })
     // for playing sound 
     var audio = new Audio("sounds/w.mp3");
